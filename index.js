da08888266f5ee0ea2f3297e819afe3fa1de9e78
davjh <dillonsdad91@gmail.com>
2022-10-16 12:45:34 -0400

    uncommmented prompts

--- index.js
+++ index.js
@@ -5,45 +5,45 @@ inquirer
 
 
   .prompt([
-// {
-//     type: `input`,
-//     message: `What is the title of your project?`,
-//     name: `title`,
-// },
+{
+    type: `input`,
+    message: `What is the title of your project?`,
+    name: `title`,
+},
 
-// {
-//     type: 'input',
-//     message: 'Provide a short description of what your project does?',
-//     name: 'Description',
-//   },
-//   {
-//     type: 'input',
-//     message: 'What are the steps to install your project?',
-//     name: 'Steps',
-//   },
-//  {
-//     type: 'input',
-//     message: 'What are some examples and instructions for use?',
-//     name: 'Examples',
-//   },
-//   {
-//     type: 'input',
-//     message: 'List your collaborators, if any, with links to their GitHub profiles.',
-//     name: 'Collabs',
-//     default: 'N/A'
-//   },
-//   {
-//     type: 'list',
-//     message: 'Which license would you like to add?',
-//     name: 'lic',
-//     choices: ['BSD', 'MIT', 'GPL'],
+{
+    type: 'input',
+    message: 'Provide a short description of what your project does?',
+    name: 'Description',
+  },
+  {
+    type: 'input',
+    message: 'What are the steps to install your project?',
+    name: 'Steps',
+  },
+ {
+    type: 'input',
+    message: 'What are some examples and instructions for use?',
+    name: 'Examples',
+  },
+  {
+    type: 'input',
+    message: 'List your collaborators, if any, with links to their GitHub profiles.',
+    name: 'Collabs',
+    default: 'N/A'
+  },
+  {
+    type: 'list',
+    message: 'Which license would you like to add?',
+    name: 'lic',
+    choices: ['BSD', 'MIT', 'GPL'],
 
-//   },
-//   {
-//     type: 'input',
-//     message: 'What is your GitHub username?',
-//     name: 'Username',
-//   },
+  },
+  {
+    type: 'input',
+    message: 'What is your GitHub username?',
+    name: 'Username',
+  },
   
   {
     type: 'input',
